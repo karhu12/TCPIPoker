f172c7002e5091bb012c83ebf7060746f5b783fa
Riku Kaipainen <shylessfly@gmail.com>
2020-11-20 12:18:32 +0200

    Changed asyncio server to use custom protocol base

--- src/tcp_ip_poker/server.py
+++ src/tcp_ip_poker/server.py
@@ -1,22 +1,31 @@
 import asyncio
+from tcp_ip_poker.poker import TexasHoldem, Player
 
-async def server_handler(reader, writer):
-    data = await reader.read(100)
-    message = data.decode()
-    addr = writer.get_extra_info('peername')
 
-    print(f"Received {message!r} from {addr!r}")
+connections = {}
+games = []
 
-    print(f"Send: {message!r}")
-    writer.write(data)
-    await writer.drain()
+class PokerGameServerProtocol(asyncio.Protocol):
+    MAX_DATA_LEN = 1024
+    ENCODE_PROTOCOL = 'utf-8'
 
-    print("Close the connection")
-    writer.close()
+    def connection_made(self, transport):
+        self.transport = transport
+        peername = transport.get_extra_info('peername')
+        print("New connection from {}".format(peername))
+        self.transport.write('Welcome to the poker game server!\nSend your username to join a lobby > '.encode(self.ENCODE_PROTOCOL))
 
+    def data_received(self, data):
+        message = data.decode()
+        addr = self.transport.get_extra_info('peername')
+
+        print(f"Received {message!r} from {addr!r}")
+        print(f"Send: {message!r}")
+        self.transport.write(data)
+        self.transport.close()
 
 async def main():
-    server = await asyncio.start_server(server_handler, '0.0.0.0', 8888)
+    server = await asyncio.start_server(PokerGameServerProtocol(), '0.0.0.0', 8888)
 
     print(f'Serving server on {server.sockets[0].getsockname()}')
     async with server:
